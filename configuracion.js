15e427d27061039a98ebb3476c44411c6c2f1fcb
cm-ss <cm@profe.colegiosanroque.org>
2026-09-24 07:36:53 +0200

    Add files via upload

---
+++ configuracion.js
@@ -0,0 +1,16 @@
+// Configuración del centro · generada con INSTALAR.html el 24/9/2026
+// Estos datos no son secretos: los necesita el navegador para conectar con Firebase.
+window.CONFIG_CENTRO = {
+  "firebase": {
+    "apiKey": "AIzaSyAnvvs38wI4B63UmIQGbf0jOOuS49gzJfY",
+    "authDomain": "cm-sanroque.firebaseapp.com",
+    "projectId": "cm-sanroque",
+    "storageBucket": "cm-sanroque.firebasestorage.app",
+    "messagingSenderId": "202279902101",
+    "appId": "1:202279902101:web:bbebc9bd0fbc600f9d349c"
+  },
+  "dominio": "profe.colegiosanroque.org",
+  "proveedor": "google",
+  "tenant": "",
+  "recaptcha": ""
+};
